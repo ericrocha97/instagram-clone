f5f110e22dab48e0e17042f9dceab29114a48b53
Eric Rocha <ericrochaeric@gmail.com>
2021-06-17 16:58:02 -0300

    aula 02

--- App.tsx
+++ App.tsx
@@ -1,21 +1,163 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  StyleSheet, 
+  Text, 
+  View, 
+  Image, 
+  TextInput, 
+  TouchableOpacity, 
+  Platform,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard
+} from 'react-native';
+
+import { FontAwesome5 } from '@expo/vector-icons'
+
+import logoImg from "./src/assets/logo.png";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.container}>
+          <StatusBar backgroundColor='#FFF' translucent={false} />
+          
+          <Image
+            source={logoImg}
+            style={styles.logo}
+          />
+
+          <TextInput 
+            placeholder="Celular, username ou email"
+            style={styles.input}
+          />
+
+          <TextInput 
+            placeholder="Sua senha"
+            style={styles.input}
+          />
+
+          <View style={styles.forgotContainer}>
+            <TouchableOpacity>
+              <Text style={styles.forgotText}>Esqueceu sua senha?</Text>
+            </TouchableOpacity>
+          </View>
+
+          <TouchableOpacity style={styles.loginButton}>
+            <Text style={styles.loginText}>Acessar</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.facebookButton}>
+            <FontAwesome5
+              name="facebook"
+              size={25}
+              color="#399FFF"
+            />
+            <Text style={styles.facebookText}>Continue como Eric Rocha</Text>
+          </TouchableOpacity>
+
+          <View style={styles.divisor}>
+            <View style={styles.divisorLine} />
+            <Text style={styles.divisorText}>OU</Text>
+            <View style={styles.divisorLine} />
+          </View>
+
+          <View style={styles.signUpContainer}>
+            <Text style={styles.signUpText}>Não possui uma conta?</Text>
+            <TouchableOpacity >
+              <Text style={styles.signUpTextButton}>Cadastre-se</Text>
+            </TouchableOpacity>
+          </View>
+          
+
+        </View>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  container: { 
     backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  logo: { 
+    marginTop: Platform.OS === 'android' ? '13%' : '20%',
+    marginBottom: Platform.OS === 'android' ? '13%' : '15%',
+  },
+  input: { 
+    width: '90%',
+    height: 42,
+    backgroundColor: '#f4f4f3',
+    marginBottom: 20,
+    padding: 8,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#e0e0e0'
+  },
+  forgotContainer: {
+    width: '90%',
+    alignItems: 'flex-end'
+  },
+  forgotText: {
+    color: '#399fff'
+  },
+  loginButton: {
+    marginTop: '5%',
+    backgroundColor:'#399fff',
+    width: '90%',
+    height: 42,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
+
+  },
+  loginText: {
+    color: '#fff',
+    fontSize: 17,
+  },
+  facebookButton: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: '15%',
+  },
+  facebookText: {
+    color: '#399fff',
+    paddingLeft: 8,
+    fontSize: 15,
+  },
+  divisor: {
+    marginTop: '10%',
+    flexDirection: 'row',
+    width: '90%',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  divisorLine: {
+    width: '45%',
+    height: 2,
+    borderRadius: 5,
+    backgroundColor: '#c4c4c4'
+  },
+  divisorText: {
+    marginHorizontal: '3%',
+    color: '#979797'
+  },
+  signUpContainer: {
+    flexDirection: 'row',
+    marginTop: '10%'
+  },
+  signUpText: {
+    color: '#979797',
+    paddingRight: 5,
+  },
+  signUpTextButton: {
+    color: '#399fff',
+    fontWeight: 'bold'
+  }
 });
---
+++ README.md
@@ -0,0 +1,69 @@
+<h1 align="center">
+    <img alt="Instagram Clone" title="Instagram Clone" src=".github/logo.png" />
+</h1>
+<h2 align="center">
+Instagram Login Clone
+</h2>
+<br>
+
+<p align="center">
+  <a href="#technologies">Technologies</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#getting-started">Getting started</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#project">Project</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#layout">Layout</a>
+</p>
+
+<p align="center">
+  <img src="https://img.shields.io/github/forks/ericrocha97/instagram-clone?label=forks&message=MIT&color=FFFFFF&labelColor=399fff" alt="Forks">     
+
+  <img src="https://img.shields.io/github/stars/ericrocha97/instagram-clone?label=stars&message=MIT&color=FFFFFF&labelColor=399fff" alt="Stars">
+</p>
+
+<br>
+
+
+---
+
+## 🧪 Technologies
+
+This project was developed using the following technologies:
+
+- [React Native](https://reactnative.dev/)
+- [Expo](https://expo.io/)
+- [TypeScript](https://www.typescriptlang.org/)
+
+## 🚀 Getting started
+
+Clone the project and access the folder
+
+```bash
+$ git clone https://github.com/ericrocha97/instagram-clone.git && cd instagram-clone
+```
+
+Follow the steps below
+```bash
+# Install the dependencies
+$ yarn install
+
+# Start the project
+$ expo start
+```
+
+## 💻 Project
+
+App Instagram login clone 📷
+
+This is a project developed during the video lesson **[Criando tela de login ](https://www.youtube.com/watch?v=rfWZGXFNO5M)** presented by
+**[Sujeito programador](https://sujeitoprogramador.com/)**.
+
+## 🔖 Layout
+
+You can view the project layout through the links below:
+
+- [Layout](https://www.figma.com/file/3u0mrXPDka3CmE8Fg6ncrx/Instagram?node-id=0%3A1) 
+
+Remembering that you need to have a [Figma](http://figma.com/) account to access it.
+
+---
+
+<p align="center">Made with 💜 by Eric Rocha</p>
